62d3476b3165885546e4097443876cb78880d189
hina50 <114747336+hina50@users.noreply.github.com>
2022-09-30 14:21:36 +0100

    Add files via upload
    Week 1 - task (cp)

---
+++ programming portfolio.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("hello hina")
+celsius = float (input("enter temprature in celsius"))
+a = 1.8
+fahrenheit = (celsius * a) + 32
+print("the temprature in fahrenheit", fahrenheit)
+var1 = 1014 - 162
+print(var1)
+var1 = 4826 / 852
+print(var1)
+var1 = 113 + 175 + 12
+print(var1)
+var1 = 300 / 24
+print(var1)
